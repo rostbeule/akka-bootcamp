d357b5c204667b94a8899d57580c1b66f504a5f5
Thomas Leininger <thomas.leininger@kardex.com>
2021-11-04 17:13:02 +0100

    completing second lesson

---
+++ src/Unit-1/DoThis/Messages.cs
@@ -0,0 +1,70 @@
+﻿namespace WinTail
+{
+    /// <summary>
+    /// Defines custom message types. (classes)
+    /// </summary>
+    public class Messages
+    {
+        #region Neutral/system messages
+
+        /// <summary>
+        /// Marker class to continue processing. (the "blank input" case)
+        /// </summary>
+        public class ContinueProcessing
+        {
+            
+        }
+        
+        #endregion
+
+        #region Success messages
+        
+        /// <summary>
+        /// Base class for signalling that user input was valid.
+        /// </summary>
+        public class InputSuccess
+        {
+            public InputSuccess(string reason)
+            {
+                Reason = reason;
+            }
+
+            public string Reason { get; private set; }
+        }
+        
+        #endregion
+
+        #region Error messages
+        
+        /// <summary>
+        /// Base class for signalling that user input was invalid.
+        /// </summary>
+        public class InputError
+        {
+            public InputError(string reason)
+            {
+                Reason = reason;
+            }
+
+            public string Reason { get; private set; }
+        }
+
+        /// <summary>
+        /// User provided blank input.
+        /// </summary>
+        public class NullInputError : InputError
+        {
+            public NullInputError(string reason) : base(reason) { }
+        }
+
+        /// <summary>
+        /// User provided invalid input (currently, input w/ odd # chars)
+        /// </summary>
+        public class ValidationError : InputError
+        {
+            public ValidationError(string reason) : base(reason) { }
+        }
+        
+        #endregion
+    }
+}
--- src/Unit-1/DoThis/Program.cs
+++ src/Unit-1/DoThis/Program.cs
@@ -3,7 +3,6 @@ using Akka.Actor;
 
 namespace WinTail
 {
-    # region Program
     class Program
     {
         public static ActorSystem MyActorSystem;
@@ -12,8 +11,6 @@ namespace WinTail
         {
             // initialize MyActorSystem
             MyActorSystem = ActorSystem.Create("MyActorSystem");
-
-            PrintInstructions();
 
             // ConsoleWriterActor
             var consoleWriterActor = MyActorSystem.ActorOf(Props.Create(() =>
@@ -24,27 +21,10 @@ namespace WinTail
                 new ConsoleReaderActor(consoleWriterActor)));
 
             // tell console reader to begin
-            consoleReaderActor.Tell("start");
+            consoleReaderActor.Tell(ConsoleReaderActor.StartCommand);
 
             // blocks the main thread from exiting until the actor system is shut down
             MyActorSystem.WhenTerminated.Wait();
         }
-
-        private static void PrintInstructions()
-        {
-            Console.WriteLine("Write whatever you want into the console!");
-            Console.Write("Some lines will appear as");
-            Console.ForegroundColor = ConsoleColor.DarkRed;
-            Console.Write(" red ");
-            Console.ResetColor();
-            Console.Write(" and others will appear as");
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.Write(" green! ");
-            Console.ResetColor();
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine("Type 'exit' to quit this application at any time.\n");
-        }
     }
-    #endregion
 }
