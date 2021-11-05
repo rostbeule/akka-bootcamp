fa7c3fa464d0696071990332e9436275f977f575
Thomas Leininger <thomas.leininger@kardex.com>
2021-11-05 14:54:14 +0100

    completing sixth lesson

--- src/Unit-1/DoThis/TailActor.cs
+++ src/Unit-1/DoThis/TailActor.cs
@@ -61,28 +61,15 @@ namespace WinTail
 
         private readonly string _filePath;
         private readonly IActorRef _reporterActor;
-        private readonly FileObserver _observer;
-        private readonly Stream _fileStream;
-        private readonly StreamReader _fileStreamReader;
+        
+        private FileObserver _observer;
+        private Stream _fileStream;
+        private StreamReader _fileStreamReader;
 
         public TailActor(IActorRef reporterActor, string filePath)
         {
             _reporterActor = reporterActor;
             _filePath = filePath;
-
-            // start watching file for changes
-            _observer = new FileObserver(Self, Path.GetFullPath(_filePath));
-            _observer.Start();
-
-            // open the file stream with shared read/write permissions
-            // (so file can be written to while open)
-            _fileStream = new FileStream(Path.GetFullPath(_filePath),
-                FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
-            _fileStreamReader = new StreamReader(_fileStream, Encoding.UTF8);
-
-            // read the initial contents of the file and send it to console as first msg
-            var text = _fileStreamReader.ReadToEnd();
-            Self.Tell(new InitialRead(_filePath, text));
         }
 
         protected override void OnReceive(object message)
@@ -109,6 +96,40 @@ namespace WinTail
                 var ir = message as InitialRead;
                 _reporterActor.Tell(ir.Text);
             }
+        }
+
+        /// <summary>
+        /// Initialization logic for actor that will tail changes to a file.
+        /// </summary>
+        protected override void PreStart()
+        {
+            // start watching file for changes
+            _observer = new FileObserver(Self, Path.GetFullPath(_filePath));
+            _observer.Start();
+
+            // open the file stream with shared read/write permissions
+            // (so file can be written to while open)
+            _fileStream = new FileStream(
+                Path.GetFullPath(_filePath), FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            _fileStreamReader = new StreamReader(_fileStream, Encoding.UTF8);
+
+            // read the initial contents of the file and send it to console as first message
+            var text = _fileStreamReader.ReadToEnd();
+            Self.Tell(new InitialRead(_filePath, text));
+        }
+        
+        /// <summary>
+        /// Cleanup OS handles for <see cref="_fileStreamReader"/> 
+        /// and <see cref="FileObserver"/>.
+        /// </summary>
+        protected override void PostStop()
+        {
+            _observer.Dispose();
+            _observer = null;
+            _fileStreamReader.Close();
+            _fileStreamReader.Dispose();
+            _fileStreamReader = null;
+            base.PostStop();
         }
     }
 }
